2fe8c22fd2a0eb6982807d6edb2c0d477bbc8770
Emanuele discala <manueldiscala@gmail.com>
2023-05-10 15:57:09 +0200

    Feat(building data)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 const {createApp} = Vue;
+
+createApp({
+    data() {
+        return {
+            skillist:[
+                {Text:"Avere una bella dose di pazienza",done:false},
+                {Text:"Essere belli organizzati",done:false},
+                {Text:"Settare una buona UI di controllo",done:false}
+            ] 
+            }
+        },
+    methods: {
+
+    }
+}).mount("#app")
