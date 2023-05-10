79546f7707eb6fe2cb17df006442fd9d1266c9c7
Emanuele discala <manueldiscala@gmail.com>
2023-05-10 17:28:57 +0200

    Added Everything sorry x low commits

--- js/main.js
+++ js/main.js
@@ -3,14 +3,40 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
+            newSkill: "",
             skillist:[
-                {Text:"Avere una bella dose di pazienza",done:false},
-                {Text:"Essere belli organizzati",done:false},
-                {Text:"Settare una buona UI di controllo",done:false}
+                {Text:"Avere una bella dose di pazienza","done":false},
+                {Text:"Essere belli organizzati","done":false},
+                {Text:"Settare una buona UI di controllo","done":false}
             ] 
             }
         },
     methods: {
-
+        // AGGIUNTA SKILL
+        addSkill() {
+            let newSkillz = {Text: this.newSkill,"done":false};
+            this.skillist.push(newSkillz);
+            this.newSkill = "";
+        },
+        // TOLTA SKILL
+        removeSkill(posizione) {
+            this.skillist.splice(posizione,1);
+        },
+        changeState(posizione) {
+            this.skillist[posizione].done = !this.skillist[posizione].done;
+            if (this.skillist[posizione].done == false) {
+                this.verity = "red"
+            }
+            else {
+                this.verity = "green";
+            }
+        },
+        conditionColor(posizione) {
+            if (this.skillist[posizione].done == false) {
+                return "red"            }
+            else {
+                return "green"
+            }
+        }    
     }
 }).mount("#app")
